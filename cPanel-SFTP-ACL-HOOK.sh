cbca62d94562ff63cee9fdc3fe5215299975db5d
Will-PSDev <williamjacksontravis@gmail.com>
2026-08-28 00:35:30 -0400

    Bug Fix for cPanel-SFTP-ACL-HOOK.sh
    Added a delay to the execusion of the script, this is needed as around 4 seconds after this script is initially triggered, cPanel will perform an action that edits file permissions and will overwrite changes made by this script if this execution isn't delayed.

--- cPanel-SFTP-ACL-HOOK.sh
+++ cPanel-SFTP-ACL-HOOK.sh
@@ -4,5 +4,8 @@ json=$(cat)
 
 homedir=$(echo "$json" | jq -r '.data.homedir')
 
+#delays execusion of script, this is needed as around 4 seconds after this script is initially triggered, cPanel will perform an action that edits file permissions and will overwrite changes made by this script if this execution isn't delayed.
+sleep 5
+
 setfacl -m g:sftpsuperuser:r-x $homedir #grants transit permissions to home
 setfacl -m g:sftpsuperuser:rwx $homedir/public_html #grants read and write to public_html
